366690d7ad8a75ec045cf206670f3c93833ebff0
batikinha <abbatikinha@gmail.com>
2023-12-27 09:45:55 -0300

    fundo fofinho tentativa 2

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.addEventListener('DOMContentLoaded', function () {
         const kawaiiBackground = document.getElementById('kawaii-background');
       
-        // Função para criar nuvens e estrelas
+        
         function createKawaiiElements() {
           for (let i = 0; i < 5; i++) {
             createCloud();
@@ -36,27 +36,27 @@ document.addEventListener('DOMContentLoaded', function () {
           }
         }
       
-        // Função para criar uma nuvem
+        
         function createCloud() {
           const cloud = document.createElement('img');
-          cloud.src = 'cloud.png'; // Substitua com o caminho para sua imagem de nuvem
+          cloud.src = 'img/nuvem.png';
           cloud.classList.add('cloud');
           cloud.style.top = `${Math.random() * 50}%`;
           cloud.style.animationDuration = `${Math.random() * 5 + 5}s`; // Duração da animação variável
           kawaiiBackground.appendChild(cloud);
         }
       
-        // Função para criar uma estrela
+       
         function createStar() {
           const star = document.createElement('div');
           star.classList.add('star');
           star.style.top = `${Math.random() * 100}%`;
           star.style.left = `${Math.random() * 100}%`;
-          star.style.animationDuration = `${Math.random() * 2 + 1}s`; // Duração da animação variável
+          star.style.animationDuration = `${Math.random() * 2 + 1}s`; 
           kawaiiBackground.appendChild(star);
         }
       
-        // Criar elementos kawaii ao carregar a página
+        
         createKawaiiElements();
     });      
   });
